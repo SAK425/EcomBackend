eb04269d7cdff2ec2f3034b1535a594a6b41d19c
SAK425 <sakshidarshane2017@gmail.com>
2021-12-13 15:47:58 +0530

    config

--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ app.use(express.json())
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/category",categoryRoutes)
 app.use("/api/v1/product",productRoutes)
+app.get('/',(req,res)=>{
+    res.send(`Server listening at ${port}`)
+})
 app.listen(port, (req, res) => {
     console.log(`Server listening at PORT ${port}`)
 })
